2a6f68c20028b26ffb02a783e5ac2f3b781d9349
Glitch (hello-webpage) <none>
2018-12-22 21:00:59 +0000

    📇🚪 Checkpoint ./mapping.js:378534/741 ./style.css:378534/19 ./index.html:378534/13

--- mapping.js
+++ mapping.js
@@ -8,11 +8,11 @@ for (let k in positions) {
   let li = document.createElement('li')
   let h2 = document.createElement('h2')
   let ckbx = document.createElement('input')
-  let p = document.createElement('p')
+  // let p = document.createElement('p')
   ckbx.type = 'checkbox'
   ckbx.id = `ck-${k}`
   h2.textContent = positions[k].name
-  p.textContent = positions[k].category
+  // p.textContent = positions[k].category
   li.appendChild(h2)
   // li.appendChild(p)
   li.appendChild(ckbx)
@@ -23,6 +23,16 @@ const checks = document.querySelectorAll('input[type="checkbox"]')
 
 for (let i = 0; i < checks.length; i++) {
   checks[i].onclick = function (e) {
-    localStorage.setItem(`position_${i}`, !!e.checked)
+    const checked = e.target.checked || false
+    
+    if (checked) {
+      localStorage.setItem(`position_${i}`, true)
+    } else {
+      localStorage.removeItem(`position_${i}`)
+    }
+  }
+  
+  if (!!localStorage.getItem(`position_${i}`)) {
+    checks[i].checked = 'checked'
   }
 }
